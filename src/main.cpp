680d6fcda566b3819847bcb04196138d61b5a3c3
Nikkk1912 <moskalevskiy1@gmail.com>
2023-10-18 23:58:11 +0300

    need to fix platform

--- src/main.cpp
+++ src/main.cpp
@@ -24,7 +24,7 @@ Vector2 Substract(Vector2 &first, const Vector2 &second) {
   first.y -= second.y;
   return first;
 }
-
+*/
 int InvertX(int currentX) {
   int newX = 0;
 
@@ -49,14 +49,16 @@ int InvertY(int currentY) {
     random_device rd;
     mt19937 gen(rd());
     uniform_int_distribution<int> distribution(0, 1);
-    currentY = distribution(gen) == 0 ? 1 : -1;
+    newY = distribution(gen) == 0 ? 1 : -1;
   }
   return newY;
 }
-*/
+
 class Ball {
+  friend class Game;
+
 private:
-  Vector2 direction = {0, 0};
+  Vector2 direction = {-1, 0};
   float speedChange = 10;
   float cellSize = 25;
   Rectangle playArea = {cellSize * 3, cellSize * 3, GetScreenWidth() - (6 * cellSize), GetScreenHeight() - (6 * cellSize)};
@@ -90,7 +92,7 @@ public:
   }
 
   void Reset() {
-    body = {playArea.width / 2, playArea.height / 2, cellSize * 2, cellSize * 2};
+    body = {playArea.width / 2, playArea.height / 2 + cellSize * 2, cellSize * 2, cellSize * 2};
     direction = {-1, 0};
   }
 
@@ -109,7 +111,6 @@ private:
   Rectangle playAreaPanel = {cellSize * 3, cellSize * 3, GetScreenWidth() - (6 * cellSize), GetScreenHeight() - (6 * cellSize)};
   Rectangle panelBody = {playAreaPanel.width - cellSize / 2.0f, playAreaPanel.height / 2.0f - cellSize * 2.0f, (float)(cellSize * 2), (float)(cellSize * 10.0)};
   int panelDirectionY = 0;
-  
 
 public:
   void Draw() {
@@ -117,7 +118,7 @@ public:
   }
 
   void Update() {
-
+    panelDirectionY = 0;
     if (cellSize != GetScreenWidth() / (1200 / 25)) {
       cellSize = GetScreenWidth() / (1200 / 25);
       float scale = (float)GetScreenWidth() / screen;
@@ -131,15 +132,8 @@ public:
       panelBody.height = (float)cellSize * 10;
       playAreaPanel = {cellSize * 3, cellSize * 3, GetScreenWidth() - (6 * cellSize), GetScreenHeight() - (6 * cellSize)};
     }
-
-    
   }
 
-  void Reset() {
-  }
-
-  Vector2 BodyCords() {
-  }
 
   int GiveDirection() { return panelDirectionY; }
 
@@ -147,43 +141,41 @@ public:
 };
 
 class Game {
-  friend class Ball;
-  friend class Panel;
-
 private:
   Ball ball = Ball();
   Panel panel = Panel();
   float cellSize = 25;
-  Rectangle playArea = {cellSize * 3, cellSize * 3, GetScreenWidth() - (6 * cellSize), GetScreenHeight() - (6 * cellSize)};;
+  Rectangle playArea = {cellSize * 3, cellSize * 3, GetScreenWidth() - (6 * cellSize), GetScreenHeight() - (6 * cellSize)};
   int loosePoints = 0;
-  float topBorder = cellSize*3;
-  Rectangle topBorder1 = {cellSize*3, cellSize * 3, playArea.width, cellSize /5};
-  Rectangle bottomBorder = {cellSize*3, playArea.height+cellSize*3-cellSize/5, playArea.width, cellSize/5};
-  Rectangle leftBorder = {cellSize*3,cellSize*3, cellSize/5,playArea.height};
-  Rectangle rightBorder = {playArea.width + cellSize*3, cellSize*3, cellSize/5, playArea.height};
-  
+  Rectangle topBorder = {cellSize * 3, cellSize * 3, playArea.width, cellSize / 5};
+  Rectangle bottomBorder = {cellSize * 3, playArea.height + cellSize * 3 - cellSize / 5, playArea.width, cellSize / 5};
+  Rectangle leftBorder = {cellSize * 3, cellSize * 3, cellSize / 5, playArea.height};
+  Rectangle rightBorder = {playArea.width + cellSize * 3, cellSize * 3, cellSize / 5, playArea.height};
+  Rectangle biggerPanel = {playArea.width - cellSize / 2.0f, playArea.height / 2.0f - cellSize * 2.0f, (float)(cellSize * 3), (float)(cellSize * 12)};
 
 public:
   void Draw() {
-    DrawRectangleRounded(topBorder1, 1, 1, RED);
-    DrawRectangleRounded(bottomBorder, 1, 1, BLUE);
-    DrawRectangleRounded(leftBorder, 1, 1, GREEN);
-    DrawRectangleRounded(rightBorder, 1, 1, YELLOW);
-    //DrawRectangleLinesEx(playArea, cellSize/5, WHITE);
+    //DrawRectangleRounded(topBorder, 1, 1, RED);
+    //DrawRectangleRounded(bottomBorder, 1, 1, BLUE);
+    //DrawRectangleRounded(leftBorder, 1, 1, GREEN);
+    //DrawRectangleRounded(rightBorder, 1, 1, YELLOW);
+    // DrawRectangleLinesEx(playArea, cellSize/5, WHITE);
     ball.Draw();
     panel.Draw();
+    DrawRectangleRounded(biggerPanel, 1, 1, WHITE);
   }
 
-  void UpdateValues(float newcellSize)
-  {
-    if (cellSize != newcellSize){
-    cellSize = newcellSize;
-    playArea = {cellSize * 3, cellSize * 3, GetScreenWidth() - (6 * cellSize), GetScreenHeight() - (6 * cellSize)};
-    topBorder = cellSize*3;
-    topBorder1 = {cellSize*3, cellSize * 3, playArea.width, cellSize /5};
-    bottomBorder = {cellSize*3, playArea.height+cellSize*3-cellSize/5, playArea.width, cellSize/5};
-    leftBorder = {cellSize*3,cellSize*3, cellSize/5,playArea.height+ cellSize*3};
+  void UpdateValues(float newcellSize) {
+    if (cellSize != newcellSize) {
+      cellSize = newcellSize;
+      playArea = {cellSize * 3, cellSize * 3, GetScreenWidth() - (6 * cellSize), GetScreenHeight() - (6 * cellSize)};
 
+      topBorder = {cellSize * 3, cellSize * 3, playArea.width, cellSize / 5};
+      bottomBorder = {cellSize * 3, playArea.height + cellSize * 3 - cellSize / 5, playArea.width, cellSize / 5};
+      leftBorder = {cellSize * 3, cellSize * 3, cellSize / 5, playArea.height + cellSize * 3};
+      leftBorder = {cellSize * 3, cellSize * 3, cellSize / 5, playArea.height};
+      rightBorder = {playArea.width + cellSize * 3, cellSize * 3, cellSize / 5, playArea.height};
+      biggerPanel = {playArea.width - cellSize / 2.0f, playArea.height / 2.0f - cellSize * 2.0f, (float)(cellSize * 3), (float)(cellSize * 12)};
     }
   }
 
@@ -203,26 +195,58 @@ public:
   int GiveLoosePoints() { return loosePoints; }
 
   void CheckCollisionWithEdgeLeft() {
+    if (CheckCollisionRecs(ball.body, leftBorder)) {
+      ball.direction.x = InvertX(ball.direction.x);
+      if (ball.direction.y == 0) {
+        random_device rd;
+        mt19937 gen(rd());
+        uniform_int_distribution<int> distribution(0, 1);
+        ball.direction.y = distribution(gen) == 0 ? 1 : -1;
+      }
+    }
   }
 
   void CheckCollisionWithEdgeRight() {
+    if (CheckCollisionRecs(ball.body, rightBorder)) {
+      ball.Reset();
+      loosePoints++;
+    }
   }
 
   void CheckCollisionWithEdgeUp() {
+    if (CheckCollisionRecs(ball.body, topBorder)) {
+      ball.direction.y = InvertY(ball.direction.y);
+    }
   }
 
   void CheckCollisionWithEdgeDown() {
+    if (CheckCollisionRecs(ball.body, bottomBorder)) {
+      ball.direction.y = InvertY(ball.direction.y);
+    }
   }
 
   void CheckCollisionWithPanel() {
+    if (CheckCollisionRecs(ball.body, biggerPanel)) {
+      cout <<"collision" << endl;
+      ball.direction.x = InvertX(ball.direction.x);
+      if (panel.panelDirectionY == 1) {
+        ball.direction.y = -1;
+      } else if(panel.panelDirectionY == -1) {
+        ball.direction.y = 1;
+      } 
+    }
   }
   void PanelChangeDirection(int direction) {
-     if (direction == -1 && panel.panelBody.y > topBorder) {
-        panel.panelBody.y += direction * panel.speedChange;
+    if (direction == -1 && panel.panelBody.y > topBorder.y + (cellSize / 5) * 2) {
+      panel.panelBody.y += direction * panel.speedChange;
+      biggerPanel.y += direction * panel.speedChange;
+      panel.panelDirectionY = 1;
     }
-    
-    else if (direction == 1 && panel.panelBody.y + (cellSize * 7) < playArea.height) {
-        panel.panelBody.y += direction * panel.speedChange;
+
+    else if (direction == 1 && panel.panelBody.y + (cellSize * 10) < bottomBorder.y - cellSize / 5) {
+      panel.panelBody.y += direction * panel.speedChange;
+      biggerPanel.y += direction * panel.speedChange;
+      panel.panelDirectionY = -1;
     }
   }
 };
@@ -252,7 +276,7 @@ int main() {
   Game game = Game();
 
   while (!WindowShouldClose()) {
-    
+
     if (IsKeyDown(KEY_W)) {
       game.PanelChangeDirection(-1);
     }
@@ -268,8 +292,8 @@ int main() {
     game.Draw();
 
     ClearBackground(dark_blue);
-    DrawLine(0, screen.height / 2, screen.width, screen.height / 2, light_yellow);
-    //DrawRectangleLinesEx(playArea, cellSize / 5, light_yellow);
+    //DrawLine(0, screen.height / 2, screen.width, screen.height / 2, light_yellow);
+    DrawRectangleLinesEx(playArea, cellSize / 5, light_yellow);
     DrawText("Pong", cellSize * 3, cellSize / 2, cellSize * 2, light_yellow);
     DrawText(TextFormat("%i", game.GiveLoosePoints()), cellSize * 3, playArea.height + cellSize * 3 + cellSize / 2, cellSize * 2, light_yellow);
     DrawText("- ball missed", cellSize * 5, playArea.height + cellSize * 3 + cellSize / 2, cellSize * 2, light_yellow);
